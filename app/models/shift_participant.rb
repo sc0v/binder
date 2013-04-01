133accfbc3f76f048e93316995b7d1d8597f0a42
Chase Brownell <minigeek@gmail.com>
2013-04-01 19:05:41 -0400

    Bad scope, reverting

--- app/models/shift_participant.rb
+++ app/models/shift_participant.rb
@@ -3,6 +3,4 @@ class ShiftParticipant < ActiveRecord::Base
   belongs_to :participant
   attr_accessible :clocked_in_at, :clocked_out_at, :shift, :participant
   validates :shift_id, :presence => true
-
-  scope :current, :where(:clocked_in_at != nil, :clocked_out_at => nil)
 end
