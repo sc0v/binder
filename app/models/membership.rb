d095d06d3fec9df633f7b501c17a473b86e0a4b2
Chase Brownell <minigeek@gmail.com>
2013-04-04 19:00:20 -0400

    Added scope for booth_chairs and validations

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,6 +2,9 @@ class Membership < ActiveRecord::Base
   belongs_to :organization
   belongs_to :participant
   attr_accessible :is_booth_chair, :title, :organization, :participant
+  validates :participant_id, :organization_id, :presence => true
   has_many :tools, :through => :checkouts
   has_many :checkouts
+
+  scope :booth_chairs, where(:is_booth_chair => true)
 end
