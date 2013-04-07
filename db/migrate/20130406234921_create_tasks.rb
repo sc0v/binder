0e90bea9e096ec05ac84f1e44b92e6c3ddfe206a
Chase Brownell <minigeek@gmail.com>
2013-04-06 20:42:47 -0400

    Task and task status model and migrations

---
+++ db/migrate/20130406234921_create_tasks.rb
@@ -0,0 +1,15 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.datetime :due_at
+      t.time :display_duration
+      t.integer :completed_by_id
+      t.string :name
+      t.text :description
+      t.references :task_status
+
+      t.timestamps
+    end
+    add_index :tasks, :task_status_id
+  end
+end
