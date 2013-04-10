9d9a085460cd55c772c7a88cc15aaab4491b3fb9
Chase Brownell <minigeek@gmail.com>
2013-04-10 01:45:27 -0400

    Task and tool changes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
     @upcoming = Task.upcoming.all
-    #@recently_completed = Task.unscoped.completed.order('updated_at DESC').limit(5).all
+    @recently_completed = Task.unscoped.not_uncompleted.order('updated_at DESC').limit(5).all
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,22 @@
 class Task < ActiveRecord::Base
   attr_accessible :completed_by, :description, :display_duration, :due_at, :name, :task_status
   validates :name, :due_at, :task_status_id, :presence => true
+  #validates :completed_by, :presence => true, :unless => :is_uncompleted?
   belongs_to :task_status
   belongs_to :completed_by, :class_name => "Participant"
 
   default_scope order('due_at DESC')
   scope :completed, where(:task_status_id => TaskStatus.find_by_name("Completed"))
   scope :uncompleted, where(:task_status_id => TaskStatus.find_by_name("Not Completed"))
+  scope :not_uncompleted, where('task_status_id <> ?', TaskStatus.find_by_name("Not Completed"))
   scope :unable_to_complete, where(:task_status_id => TaskStatus.find_by_name("Unable To Complete"))
   scope :upcoming, lambda{ where("display_duration < ? and task_status_id = ?", Time.now, TaskStatus.find_by_name("Not Completed")) }
 
   def is_past_due
     return self.due_at < Time.now
+  end
+
+  def is_uncompleted
+    return self.task_status == TaskStatus.find_by_name("Not Completed")
   end
 end
