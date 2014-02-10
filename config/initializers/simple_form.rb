d97c3d2d6f6b61761711a056e4d8ca15f53e98a9
Chase Brownell <minigeek@gmail.com>
2014-02-09 19:55:07 -0500

    Hack Simple Forms to work with Bootstrap 3

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -95,10 +95,10 @@ SimpleForm.setup do |config|
   # config.label_text = lambda { |label, required| "#{required} #{label}" }
 
   # You can define the class to use on all labels. Default is nil.
-  config.label_class = 'control-label'
+  config.label_class = 'col-sm-2 col-lg-2 control-label'
 
   # You can define the class to use on all forms. Default is simple_form.
-  # config.form_class = :simple_form
+  config.form_class = 'simple_form form-horizontal'
 
   # You can define which elements should obtain additional classes
   # config.generate_additional_classes_for = [:wrapper, :label, :input]
