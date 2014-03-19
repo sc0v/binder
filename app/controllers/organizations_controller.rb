552adc24b9ddf7bdff3dde52af4a6a6b38d344aa
Chase Brownell <minigeek@gmail.com>
2014-03-19 01:19:59 -0400

    Add Short Name to Org Form

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -88,7 +88,7 @@ class OrganizationsController < ApplicationController
   private
 
   def organization_params
-    params.require(:organization).permit(:name, :organization_category_id)
+    params.require(:organization).permit(:name, :short_name, :organization_category_id)
   end
 end
 
