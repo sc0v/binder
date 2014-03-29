550bc0669ce8c76ab1c82239b341a57152e69872
Chase Brownell <minigeek@gmail.com>
2014-03-29 12:16:40 -0400

    Cleanup OmniAuth Callbacks

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def shibboleth
     @user = User.find_for_shibboleth_oauth(request, current_user)
-# if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Shibboleth") if is_navigational_format?
-      session[:name] = @user.name
-# else
-# session["devise.shibboleth_data"] = request.env["omniauth.auth"]
-# redirect_to new_user_registration_url
-# end
+    sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+    set_flash_message(:notice, :success, :kind => "Shibboleth") if is_navigational_format?
   end
 end
