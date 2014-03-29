5600d988dd44748e5d8e7c7fe3f51687c4f4c727
Chase Brownell <minigeek@gmail.com>
2014-03-29 12:14:43 -0400

    Potential Fix for login bug

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,9 +2,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def shibboleth
     @user = User.find_for_shibboleth_oauth(request, current_user)
 # if @user.persisted?
-      session[:name] = @user.particpant.name
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Shibboleth") if is_navigational_format?
+      session[:name] = @user.name
 # else
 # session["devise.shibboleth_data"] = request.env["omniauth.auth"]
 # redirect_to new_user_registration_url
