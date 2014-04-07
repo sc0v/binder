b78514e630ed9c33911dfebaacd0f9452e5a8478
Chase Brownell <minigeek@gmail.com>
2014-04-07 17:23:16 -0400

    Fix Org Status Caching

--- app/models/organization_status.rb
+++ app/models/organization_status.rb
@@ -25,7 +25,7 @@ class OrganizationStatus < ActiveRecord::Base
   validates_associated :organization_status_type, :organization, :participant
 
   belongs_to :organization_status_type
-  belongs_to :organization
+  belongs_to :organization, :touch => true
   belongs_to :participant
 
   scope :displayable, -> { joins(:organization_status_type).where('organization_status_types.display = ?', true) }
