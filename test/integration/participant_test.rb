2bfc38d547030154ccefd74cdb0f64ae69fd34ce
Patrick Koenig <pkoenig10@gmail.com>
2016-04-05 02:45:25 -0400

    Removed integration test using Card Service call

--- test/integration/participant_test.rb
+++ test/integration/participant_test.rb
@@ -4,9 +4,6 @@ class ParticipantTest < ActiveSupport::TestCase
 
   context "With a refreshed ldap cache, " do
     setup do
-      # Webmock request to card lookup since it doesn't work from random machines
-      stub_request(:any, /.*merichar-dev\.eberly\.cmu\.edu.*/).to_return(:body => '{ "andrewid": "meribyte", "expiration": "2013-11-29T00:00:00+00:00" }', :status => 200, :headers => { 'Content-Length' => 17 })
-
       @participant = FactoryGirl.create(:participant, :andrewid => "meribyte")
     end
 
@@ -16,10 +13,6 @@ class ParticipantTest < ActiveSupport::TestCase
     should "check that participant factory object is created and can recieve message calls" do
       @participant.send(:update_cache)
       assert true
-    end
-
-    should "check that student object is the same object as the Participant lookup via card number (This depends on the idcard lookup working)" do
-      assert_equal @participant, Participant.find_by_card("811825505")
     end
 
     should "return name from directory" do
