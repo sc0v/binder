414de93ee4298b60f19635185897d4879dcd2aeb
Bruce Lin <ruiyangl@andrew.cmu.edu>
2017-02-26 14:46:57 -0500

    created unit test for organization model. almost done

--- test/unit/organization_test.rb
+++ test/unit/organization_test.rb
@@ -21,6 +21,7 @@
 
 require 'test_helper'
 
+
 class OrganizationTest < ActiveSupport::TestCase
   # Relationships
   should belong_to(:organization_category)
@@ -42,7 +43,7 @@ class OrganizationTest < ActiveSupport::TestCase
 
   context "With a proper context, " do
     setup do
-      @short_org = FactoryGirl.create(:organization, :name => 'longer name', :short_name => 'name')
+      @short_org = FactoryGirl.create(:organization, :name => 'short name', :short_name => 'name')
       @long_org = FactoryGirl.create(:organization, :name => 'long name', :short_name => nil)
     end
 
@@ -54,11 +55,38 @@ class OrganizationTest < ActiveSupport::TestCase
     end
 
     # Scopes
+    # only_categories
+    should "show that the only_categories functions correctly" do
+      short_org_category = @short_org.organization_category.name
+      long_org_category = @long_org.organization_category.name
+      assert_equal ['short name'], Organization.only_categories(short_org_category).map {|org| org.name}
+      assert_equal ['long name'], Organization.only_categories(long_org_category).map {|org| org.name}
+    end
+
+    # search
+    should "show that the search scope functions correcly" do
+      assert_equal ['long name'], Organization.search('long name').map {|org| org.name }
+      assert_equal ['long name', 'short name'], Organization.search('name').map {|org| org.name}
+    end
+
+    # ---------------------------- End of scope testing
 
     # Methods
     should 'have a short_name method' do
       assert_equal('name', @short_org.short_name)
       assert_equal('long name', @long_org.short_name)
+    end
+
+    should 'give back the correct booth chair for this organization' do
+      chair_person = FactoryGirl.create(:participant)
+      short_chair = FactoryGirl.create(:membership,
+                   is_booth_chair: true,
+                   organization: @short_org,
+                   participant: chair_person)
+      # random_part.update_attribute(:is_booth_chair, true)
+      # random_part.update_attribute(:organization, @short_org)
+      assert_equal [chair_person.andrewid], @short_org.booth_chairs.map { |bc| bc.andrewid }
+      assert_equal [], @long_org.booth_chairs
     end
   end
 end
