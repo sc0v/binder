5e9667b4148dba3ddc928ccc9523efef18b0689a
Rumby Wilson <erwilson@andrew.cmu.edu>
2017-02-26 17:14:04 -0500

    Document unit tests done, besides one clarification issue with search scope

--- test/unit/document_test.rb
+++ test/unit/document_test.rb
@@ -22,10 +22,29 @@ class DocumentTest < ActiveSupport::TestCase
   # Relationships
   should belong_to(:organization)
 
-  # Validations- how ot test validates_associated
+  context "With a proper context, " do
+    setup do
+      @org = FactoryGirl.create(:organization_alias, :id => 123)
+      @document = FactoryGirl.create(:document, :organization_id => @org.id, :name => "Booth")
+      @document2 = FactoryGirl.create(:document, :organization_id => @org.id, :name => "Electric")
+    end
 
-  # Scopes
+    teardown do
+    end
 
-  # Methods
+    # Validations- is there a better way to test this? 
+    should "show that a document has an organization assocaited with it" do
+      assert_equal 123,  @document.organization_id
+    end
+
+    #scopes 
+    should "show that search scope works" do
+    	@doc = Document.search('booth')
+    #WHen acutally testing you can see the name, but will default back to document when asserting it for some reason
+      assert_equal 'Booth', @doc
+
+    end
+    
+  end
 
 end
