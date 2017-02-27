6a2477edabe3fe44e72b6c5f6474f693f7ef3694
Bruce Lin <ruiyangl@andrew.cmu.edu>
2017-02-27 10:39:03 -0500

    fixed error! added organization_status to the factory file. test for organization_status now passed!

--- test/unit/organization_status_test.rb
+++ test/unit/organization_status_test.rb
@@ -34,37 +34,37 @@ class OrganizationStatusTest < ActiveSupport::TestCase
   should validate_presence_of(:organization)
   should validate_presence_of(:participant)
 
-  # context "With a proper context, " do
-  #   setup do
-  #     @status_type1 = FactoryGirl.create(:organization_status_type)
-  #     @status_type2 = FactoryGirl.create(:organization_status_type, :display => true)
-  #     @org = FactoryGirl.create(:organization)
-  #     @p = FactoryGirl.create(:participant)
-  #     @status1 = FactoryGirl.create(:organization_status,
-  #               :organization_status_type => @status_type1,
-  #               :organization => @org, :participant => @p)
-  #     @status2 = FactoryGirl.create(:organization_status,
-  #               :organization_status_type => @status_type2,
-  #               :organization => @org, :participant => @p)
-  #   end
-  #
-  #   teardown do
-  #     @status_type1 = nil
-  #     @status_type2 = nil
-  #     @org = nil
-  #     @p = nil
-  #     @status1 = nil
-  #     @status2 = nil
-  #   end
-  #
-  #   should "show that all factories are properly created" do
-  #     assert_equal 2, OrganizationStatus.all.size
-  #   end
-  #
-  #   # Scope
-  #   should "display statuses that are only displayable" do
-  #     assert_equal 1, OrganizationStatus.displayable.size
-  #   end
-  # end
+  context "With a proper context, " do
+    setup do
+      @status_type1 = FactoryGirl.create(:organization_status_type)
+      @status_type2 = FactoryGirl.create(:organization_status_type, :display => true)
+      @org = FactoryGirl.create(:organization)
+      @p = FactoryGirl.create(:participant)
+      @status1 = FactoryGirl.create(:organization_status,
+                :organization_status_type => @status_type1,
+                :organization => @org, :participant => @p)
+      @status2 = FactoryGirl.create(:organization_status,
+                :organization_status_type => @status_type2,
+                :organization => @org, :participant => @p)
+    end
+
+    teardown do
+      @status_type1 = nil
+      @status_type2 = nil
+      @org = nil
+      @p = nil
+      @status1 = nil
+      @status2 = nil
+    end
+
+    should "show that all factories are properly created" do
+      assert_equal 2, OrganizationStatus.all.size
+    end
+
+    # Scope
+    should "display statuses that are only displayable" do
+      assert_equal 1, OrganizationStatus.displayable.size
+    end
+  end
 
 end
