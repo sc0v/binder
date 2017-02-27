9297b13a58aa370128dd2ce51955984fc3df1651
Akshay Goradia <asgoradia@gmail.com>
2017-02-26 23:05:41 -0500

    Created Units tests for Charge model

--- test/unit/charge_test.rb
+++ test/unit/charge_test.rb
@@ -36,19 +36,36 @@ class ChargeTest < ActiveSupport::TestCase
 
   # Validations
 
+  should validate_presence_of(:charged_at)
+  should validate_presence_of(:issuing_participant)
+  should validate_presence_of(:organization)
+  should validate_presence_of(:charge_type)
+  should validate_presence_of(:amount)
+
+  should validate_numericality_of(:amount)
 
   # Methods
   context "With a proper context, " do
     setup do
       # Create a charges
-      @fine = FactoryGirl.create(:charge)
+      @fine = FactoryGirl.create(:charge, :is_approved => false)
+      @fine2 = FactoryGirl.create(:charge, :is_approved => false)
+      @fine3 = FactoryGirl.create(:charge, :is_approved => true)
     end
 
     teardown do
     end
 
     should "show that all factories are properly created" do
-      assert_equal 1, Charge.all.size
+      assert_equal 3, Charge.all.size
+    end
+
+    should "show that pending scope works" do
+      assert_equal 2, Charge.pending.size
+    end
+
+    should "show that approved scope works" do
+      assert_equal 1, Charge.approved.size
     end
 
     context "Testing charges" do
