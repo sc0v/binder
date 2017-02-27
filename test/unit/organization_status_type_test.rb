090922944f8d9e6c28379fdf3d0c4817e10f5126
Bruce Lin <ruiyangl@andrew.cmu.edu>
2017-02-27 14:51:35 -0500

    fixed previous file name error. Test for organization_status_type passed

--- test/unit/organization_status_type_test.rb
+++ test/unit/organization_status_type_test.rb
@@ -22,34 +22,48 @@
 # **`name`**     | `string(255)`      |
 #
 
-class OrganizationStatusTypeTest < ActiveRecord::TestCase
+require 'test_helper'
+
+class OrganizationStatusTypeTest < ActiveSupport::TestCase
+
   # Relationships
   should have_many(:organization_statuses)
-  should have_many(:organizations)
-  should have_many(:participants).through(:organization_statuses)
+
+  # Validations
+  should validate_presence_of(:name)
+  should validate_uniqueness_of(:name)
 
   context "With a proper context, " do
     setup do
       @type1 = FactoryGirl.create(:organization_status_type)
+      @person = FactoryGirl.create(:participant)
+      @org = FactoryGirl.create(:organization)
+      @status = FactoryGirl.create(:organization_status,
+                                  participant: @person, organization: @org, organization_status_type: @type1)
     end
 
     teardown do
       @type1 = nil
+      @person = nil
+      @org = nil
+      @status = nil
     end
-
-
+    #
     should "show that all factories are properly created" do
       assert_equal 1, OrganizationStatusType.all.size
     end
 
     # Dependency
-    should "show that status is destroyed if status type is destroyed" do
-      org = FactoryGirl.create(:organization)
-      person = FactoryGirl.create(:participant)
-      status = FactoryGirl.create(:organization_status,
-              organization: org, organization_status_type: @type1, participant: person)
+    should "delete status if status type is destroyed" do
+
+
+      @type1.update_attribute(:display, true)
+      assert(@status.valid?)
+      assert_equal 1, OrganizationStatus.all.size
+
       @type1.destroy
-      assert status.destroyed?
+      assert_equal 0, OrganizationStatus.all.size
     end
   end
+
 end
