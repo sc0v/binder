bb6e6bdd551db21ac844387c1f4462a681ae2077
Rumby Wilson <erwilson@andrew.cmu.edu>
2017-03-05 17:34:28 -0500

    Cleaned code (moved authorization from views to controllers) for charges (now participants can't read charges pages from views)

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,5 +1,32 @@
+# ## Schema Information
+#
+# Table name: `charges`
+#
+# ### Columns
+#
+# Name                            | Type               | Attributes
+# ------------------------------- | ------------------ | ---------------------------
+# **`amount`**                    | `decimal(8, 2)`    |
+# **`charge_type_id`**            | `integer`          |
+# **`charged_at`**                | `datetime`         |
+# **`created_at`**                | `datetime`         |
+# **`creating_participant_id`**   | `integer`          |
+# **`description`**               | `text(65535)`      |
+# **`id`**                        | `integer`          | `not null, primary key`
+# **`is_approved`**               | `boolean`          |
+# **`issuing_participant_id`**    | `integer`          |
+# **`organization_id`**           | `integer`          |
+# **`receiving_participant_id`**  | `integer`          |
+# **`updated_at`**                | `datetime`         |
+#
+# ### Indexes
+#
+# * `index_charges_on_organization_id`:
+#     * **`organization_id`**
+#
+
 class ChargesController < ApplicationController
-  load_and_authorize_resource skip_load_resource only: [:create] 
+  load_and_authorize_resource 
   before_action :set_charge, only: [:show, :edit, :update, :destroy, :approve]
 
   # GET /charges
