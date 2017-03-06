be54814c93e07044f6cc2fb53bb8e5f0b3769186
Akshay <akshaygoradia@yahoo.com>
2017-03-06 16:31:30 -0500

    Added Dependency Tests for Participant, Coverage for Other Models & Bugfix (#237)
    * Added tests for charge_type model
    
    * Added tests for role model
    
    * Added tests for StoreItem model
    
    * Added tests for StorePurchase model
    
    * Added tests for Checkout model
    
    * Fixed charge model coverage
    
    * Added tests to charge_type, checkout, and partly participant
    
    * Added full test coverage for Task model
    
    * Added fixes to participant model tests
    
    * Added dependency tests for participant
    
    * Fixed duplicate cart items bug
    
    * Edited factory file to get rid of merge conflict
    
    * Added schema.rb file

--- test/unit/checkout_test.rb
+++ test/unit/checkout_test.rb
@@ -35,18 +35,33 @@ class CheckoutTest < ActiveSupport::TestCase
 
   context "With a proper context, " do
     setup do
-      FactoryGirl.create(:checkout)
+      @org = FactoryGirl.create(:organization, name: "bruce")
+      @person = FactoryGirl.create(:participant)
+      @type1 = FactoryGirl.create(:tool_type, name: "hammer")
+      @type2 = FactoryGirl.create(:tool_type, name: "drill")
+      @type3 = FactoryGirl.create(:tool_type, name: "lmao")
+      @t1 = FactoryGirl.create(:tool, barcode: 1111, tool_type: @type1)
+      @t2 = FactoryGirl.create(:tool, barcode: 2222, tool_type: @type2)
+      @t3 = FactoryGirl.create(:tool, barcode: 3333, tool_type: @type3)
+      @checkout1 = FactoryGirl.create(:checkout, :checked_in_at => Time.now, organization: @org, participant: @person, tool: @t1)
+      @checkout2 = FactoryGirl.create(:checkout, :checked_in_at => Time.now, organization: @org, participant: @person, tool: @t2)
+      @checkout3 = FactoryGirl.create(:checkout, :checked_in_at => nil, organization: @org, participant: @person, tool: @t3)
     end
 
     teardown do
     end
 
     should "show that all factories are properly created" do
-      assert_equal 1, Checkout.all.size
+      assert_equal 3, Checkout.all.size
     end
 
     # Scopes
+    should "show that current scope works" do
+      assert_equal 1, Checkout.current.size
+    end
 
-    # Methods
+    should "show that old scope works" do
+      assert_equal 2, Checkout.old.size
+    end
   end
 end
