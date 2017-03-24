3135ef7336b5b2d9646e4e4b8faac7528895348d
Stephen Clark <sclark@linux.com>
2017-03-24 14:34:41 -0400

    Using migration technique from http://stackoverflow.com/questions/17150529

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,8 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+ActiveRecord::Migration.maintain_test_schema!
+
 class ActiveSupport::TestCase
 
   def deny(condition)
