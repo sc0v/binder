27b44a88692a04e4dcbae3ffd7d91811acb6953a
Bruce Lin <ruiyangl@andrew.cmu.edu>
2017-03-27 17:03:25 -0400

    added code to send out text alert to the next person on the wait list after a tool is checked back in

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -22,6 +22,8 @@
 #
 
 class Checkout < ActiveRecord::Base
+  include Messenger
+
   # For lookups
   def card_number=( card_number )
     @card_number = card_number
@@ -35,6 +37,7 @@ class Checkout < ActiveRecord::Base
   validates_associated :tool, :organization, :participant
 
   before_save :checked_out_at, :presence => true
+  after_update :notify
 
   belongs_to :participant, :touch => true
   belongs_to :organization, :touch => true
@@ -44,4 +47,20 @@ class Checkout < ActiveRecord::Base
   scope :old, -> { where('checked_in_at IS NOT NULL') }
   scope :current, -> { where('checked_in_at IS NULL') }
 
+  private
+
+  def notify
+    if (self.checked_in_at != nil)
+      toolCategory = self.tool.tool_type
+      waitlist = ToolWaitlist.for_tool_type(toolCategory.id).by_wait_start_time
+      if (waitlist.count != 0)
+        nextPerson = waitlist.first.participant
+        number = nextPerson.phone_number
+        puts ("\n\n\nThis guy is: #{nextPerson.name}")
+        puts ("\n\n\nThis guy's number is: #{number}\n\n\n")
+        content = "#{toolCategory.name} is now available at the trailer. Please come pick it up within 5 minutes!"
+        send_sms(number, content)
+      end
+    end
+  end
 end
