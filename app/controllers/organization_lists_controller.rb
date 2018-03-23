5604d85b193b49e0b364ed41258858601ffc23d1
Andrew Kim <akim1@andrew.cmu.edu>
2018-03-23 16:26:39 -0400

    booth chairs have their org by default when uploading members, admins still have dropdown for all orgs

--- app/controllers/organization_lists_controller.rb
+++ app/controllers/organization_lists_controller.rb
@@ -24,6 +24,13 @@ class OrganizationListsController < ApplicationController
   def index
     @user = current_user
     @members = OrganizationList.all
+    @user.participant.memberships.each do |membership|
+      if membership.is_booth_chair?
+        @org_name = membership.organization_name_formatted
+        @org_name = @org_name.slice(0, @org_name.length-14)
+      end
+    end
+
   end
 
   # no need for show page
