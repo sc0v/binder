94fdb51c603cbb6b53950ce994caabd9173c6d9c
Karan Bokil <kbokil@andrew.cmu.edu>
2018-03-23 20:07:23 +0000

    Added seed method to cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -99,6 +99,18 @@ namespace :db do
     end
   end
   before 'db:drop', 'bundler:install'
+  
+    desc 'Runs rake db:seed'
+  task :seed do
+    on roles(:db) do
+      within release_path do
+        with rails_env: (fetch(:rails_env) || fetch(:stage)) do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+  before 'db:seed', 'bundler:install'
 
   desc 'Runs rake db:setup'
   task :setup do
