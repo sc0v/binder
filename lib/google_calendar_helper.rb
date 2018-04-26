b83b0a45d90359d87b7f7cfa981c08162c50895a
Andrew Kim <akim1@andrew.cmu.edu>
2018-04-26 19:24:12 -0400

    calendar api working with scc binder account connected

--- lib/google_calendar_helper.rb
+++ lib/google_calendar_helper.rb
@@ -5,13 +5,13 @@ require 'google/apis/calendar_v3'
 class GoogleCalendarHelper
   attr_reader :calendar
   SCOPE = ["https://www.googleapis.com/auth/calendar"]
-  CALENDAR_ID = 'sccakim1@gmail.com'
+  CALENDAR_ID = 'springcarnival.org_n1fd051prdia7jg2qfcbbco8oc@group.calendar.google.com'
   
   def initialize
     @calendar = Google::Apis::CalendarV3::CalendarService.new
     # get_application_default forces the use of this environment variable.
     # Set it to the location of your JSON credentials.
-    ENV["GOOGLE_APPLICATION_CREDENTIALS"] = "lib/keyfile.json"
+    # ENV["GOOGLE_APPLICATION_CREDENTIALS"] = "lib/keyfile.json"
     @calendar.authorization = Google::Auth.get_application_default(SCOPE)
   end
 end
