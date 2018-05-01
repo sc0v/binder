5b02f8e10412e84b81ef7677bdc84ed0091992dd
Andrew Kim <akim1@andrew.cmu.edu>
2018-05-01 17:23:56 -0400

    Added comment to google calendar helper about changing the calendar ID

--- lib/google_calendar_helper.rb
+++ lib/google_calendar_helper.rb
@@ -3,13 +3,18 @@ require 'google/apis/calendar_v3'
 
 # This code was used from http://cirefice.me/software%20development/integrating-rails-with-google-apis 
 # Credit goes to Christopher S. Cirefice
+
 class GoogleCalendarHelper
   attr_reader :calendar
   SCOPE = ["https://www.googleapis.com/auth/calendar"]
+  # this calendar ID should correspond with the ID of the calendar you want to 
+  # pull events from, check step 7 in docs/googleCalendarTutorial.pdf to learn
+  # how to find the ID of your google calendar
   CALENDAR_ID = 'sccakim1@gmail.com'
   
   def initialize
     @calendar = Google::Apis::CalendarV3::CalendarService.new
+
     # get_application_default forces the use of this environment variable.
     # Set it to the location of your JSON credentials.
     ENV["GOOGLE_APPLICATION_CREDENTIALS"] = "lib/keyfile.json"
