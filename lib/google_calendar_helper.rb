9f87d97b0e757bf2492409c0062d3add2b40e1e5
Andrew Kim <akim1@andrew.cmu.edu>
2018-05-03 18:43:55 -0400

    changed google calendar account to SCC's

--- lib/google_calendar_helper.rb
+++ lib/google_calendar_helper.rb
@@ -10,14 +10,14 @@ class GoogleCalendarHelper
   # this calendar ID should correspond with the ID of the calendar you want to 
   # pull events from, check step 7 in docs/googleCalendarTutorial.pdf to learn
   # how to find the ID of your google calendar
-  CALENDAR_ID = 'sccakim1@gmail.com'
+  CALENDAR_ID = 'springcarnival.org_n1fd051prdia7jg2qfcbbco8oc@group.calendar.google.com'
   
   def initialize
     @calendar = Google::Apis::CalendarV3::CalendarService.new
 
     # get_application_default forces the use of this environment variable.
     # Set it to the location of your JSON credentials.
-    ENV["GOOGLE_APPLICATION_CREDENTIALS"] = "lib/keyfile.json"
+    # ENV["GOOGLE_APPLICATION_CREDENTIALS"] = "lib/keyfile.json"
     @calendar.authorization = Google::Auth.get_application_default(SCOPE)
   end
 end
