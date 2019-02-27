12f03924621bf172453f000acc0398c2ef0dc6cd
Lorraine Zhang <lorrainz@andrew.cmu.edu>
2019-02-27 20:50:06 +0000

    recreated delayed jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190227041337) do
+ActiveRecord::Schema.define(version: 20190227204757) do
 
   create_table "certification_types", force: :cascade do |t|
     t.string   "name",       limit: 255
