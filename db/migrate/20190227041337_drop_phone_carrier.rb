2f45ebdd4049598fa060cf6f8ce14f3a77d955c1
Lorraine Zhang <lorrainz@andrew.cmu.edu>
2019-03-06 05:51:24 +0000

    removed redundant index removal on phonecarrier drop migration

--- db/migrate/20190227041337_drop_phone_carrier.rb
+++ db/migrate/20190227041337_drop_phone_carrier.rb
@@ -1,8 +1,7 @@
 class DropPhoneCarrier < ActiveRecord::Migration
   def change
     remove_foreign_key :participants, :phone_carrier
-    remove_column :participants, :phone_carrier_id
-    remove_index :participants, :name => "index_participants_on_phone_carrier_id"
+    remove_reference :participants, :phone_carrier
     drop_table :phone_carriers
   end
 end
