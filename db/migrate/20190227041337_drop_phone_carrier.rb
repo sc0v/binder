14721008603587cad5a724d1aff593cd47b29dc8
Lorraine Zhang <lorrainz@andrew.cmu.edu>
2019-03-06 03:59:46 +0000

    modified migrations to drop phone carrier references in participants table and remigrated

--- db/migrate/20190227041337_drop_phone_carrier.rb
+++ db/migrate/20190227041337_drop_phone_carrier.rb
@@ -1,5 +1,7 @@
 class DropPhoneCarrier < ActiveRecord::Migration
   def change
     drop_table :phone_carriers
+    delete_column :participants, :phone_carrier_id
+    remove_index :participants, :name => "index_participants_on_phone_carrier_id"
   end
 end
