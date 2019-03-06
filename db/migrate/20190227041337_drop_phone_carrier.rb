ffc0d161b2d00de210afbbb51d7a60803b92b325
Lorraine Zhang <lorrainz@andrew.cmu.edu>
2019-03-06 05:14:57 +0000

    fixed migration

--- db/migrate/20190227041337_drop_phone_carrier.rb
+++ db/migrate/20190227041337_drop_phone_carrier.rb
@@ -1,7 +1,6 @@
 class DropPhoneCarrier < ActiveRecord::Migration
   def change
+    remove_foreign_key :participants, :phone_carrier
     drop_table :phone_carriers
-    delete_column :participants, :phone_carrier_id
-    remove_index :participants, :name => "index_participants_on_phone_carrier_id"
   end
 end
