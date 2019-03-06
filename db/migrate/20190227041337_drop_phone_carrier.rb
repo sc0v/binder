2aa3e28bff8c898a20aa58b1f5e45842523f4e71
Lorraine Zhang <lorrainz@andrew.cmu.edu>
2019-03-06 05:43:08 +0000

    corrected index removal from DropPhoneCarrier migration

--- db/migrate/20190227041337_drop_phone_carrier.rb
+++ db/migrate/20190227041337_drop_phone_carrier.rb
@@ -2,7 +2,7 @@ class DropPhoneCarrier < ActiveRecord::Migration
   def change
     remove_foreign_key :participants, :phone_carrier
     remove_column :participants, :phone_carrier_id
-    remove_index :participants, :name => "index_organization_timeline_entries_on_organization_id"
+    remove_index :participants, :name => "index_participants_on_phone_carrier_id"
     drop_table :phone_carriers
   end
 end
