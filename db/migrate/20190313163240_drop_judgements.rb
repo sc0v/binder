911ef9087006ab1435dc6515c63e2a651e1b76d0
Siri Tembunkiart <Aklaran@users.noreply.github.com>
2019-03-21 18:17:21 -0400

    drops judges, judgement-categories, and judgements (#286)

---
+++ db/migrate/20190313163240_drop_judgements.rb
@@ -0,0 +1,10 @@
+class DropJudgements < ActiveRecord::Migration
+  def change
+    remove_reference :judgements, :judge
+    remove_reference :judgements, :judgement_category
+
+    drop_table :judges
+    drop_table :judgement_categories
+    drop_table :judgements
+  end
+end
