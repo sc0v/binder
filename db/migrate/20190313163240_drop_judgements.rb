aca66969ff305cb39ca36300e080511df1ffb4e2
Lorraine Zhang <lorrainz@andrew.cmu.edu>
2019-03-22 20:29:43 +0000

    undid judgement foreign key removal

--- db/migrate/20190313163240_drop_judgements.rb
+++ db/migrate/20190313163240_drop_judgements.rb
@@ -1,7 +1,5 @@
 class DropJudgements < ActiveRecord::Migration
   def change
-    remove_foreign_key :judgements, :judge
-    remove_foreign_key :judgements, :judgement_category
     
     remove_reference :judgements, :judge
     remove_reference :judgements, :judgement_category
