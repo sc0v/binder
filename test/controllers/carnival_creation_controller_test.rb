2e182cf3e98a4ad45af9933f8946c3d74d1b62af
Bo Tembunkiart <stembunk@andrew.cmu.edu>
2019-03-29 16:06:31 -0400

    updates carnival_creation_controller_test

--- test/controllers/carnival_creation_controller_test.rb
+++ test/controllers/carnival_creation_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class CarnivalCreationControllerTest < ActionController::TestCase
-  test "should get showUploader" do
-    get :showUploader
+  test "should get show_uploader" do
+    get :show_uploader
     assert_response :success
   end
 
-  test "should get showDiff" do
-    get :showDiff
+  test "should get show_diff" do
+    get :show_diff
     assert_response :success
   end
 
