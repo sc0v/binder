679d7ba6fa68129a8a7ea6042a6827dcc079fe02
Stephen Clark <saclark@cmu.edu>
2019-04-02 00:21:01 -0400

    Add slack messenger (#296)
    * slack: add slack bot for queues
    
    * slack: add slack bot for queues
    
    * Update organization_timeline_entry.rb

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -42,4 +42,18 @@ module Messenger
     end
   end
 
+  def send_slack(channel, text)
+    # Send a message using the slack API: https://api.slack.com/methods/chat.postMessage
+    slack_token = ENV["SLACK_BOT_TOKEN"]
+    
+    if (not Rails.env.test?) and (not slack_token.nil?)
+      uri = URI.parse('https://slack.com/api/chat.postMessage')
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+      req.body = {channel: channel, text: text, token: slack_token}.to_json
+      http.request(req)
+    end
+  end
+
 end
