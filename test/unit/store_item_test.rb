64ee7a8064f91970ed4d47b4cf18d54d722a71d3
Lorraine Zhang <lorrainz@andrew.cmu.edu>
2019-04-17 03:45:18 +0000

    deleted tests for store item quantity

--- test/unit/store_item_test.rb
+++ test/unit/store_item_test.rb
@@ -28,7 +28,7 @@ class StoreItemTest < ActiveSupport::TestCase
   context "With a proper context, " do
     setup do
       # Create store 
-      @store_item = FactoryGirl.create(:store_item, :name => "Hammer", :price => 20, :quantity => 5)
+      @store_item = FactoryGirl.create(:store_item, :name => "Hammer", :price => 20, :in_stock => true)
       # Create charge 
       @charge = FactoryGirl.create(:charge, :is_approved => true)
       # Create store_purchase 
@@ -38,8 +38,5 @@ class StoreItemTest < ActiveSupport::TestCase
     teardown do
     end
 
-    should "show that quantity_available method works correctly" do
-      assert_equal 4, @store_item.quantity_available 
-    end
   end
 end
