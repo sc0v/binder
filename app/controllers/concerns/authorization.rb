6911ea2fda5bef3fe884efe6a2873dadf374a780
Meg Richards <merichar@merichar.org>
2023-03-20 01:37:56 -0400

    Update Authorization Concern
     * Change current_ability to be a Current attribute
     * Evaluate Current.ability on inclusion
     * Match authentication concern method name

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -3,10 +3,17 @@
 module Authorization
   extend ActiveSupport::Concern
 
+  included do
+    before_action :set_current_ability
+  end
+
   private
 
-  # For use by cancancan
-  def current_ability
-    @current_ability ||= Ability.new(Current.user)
+  # Set current user's abilities (current user may be nil)
+  def set_current_ability
+    Current.ability ||= Ability.new(Current.user)
   end
+
+  # CanCanCan requires a current_ability method
+  alias current_ability set_current_ability
 end
