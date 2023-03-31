e27dc8101dec54e051cfa747d29a0f795fce7971
Meg Richards <merichar@merichar.org>
2023-03-31 13:26:06 -0400

    Fix I18n & Rubocop Errors in Sessions Controller
    Also add a direct_link in routes for login_redirect, which takes an
    origin but returns root_url if it's nil.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,30 @@
 # frozen_string_literal: true
-
 class SessionsController < ApplicationController
   def create
-    u = Participant.from_omniauth(request.env['omniauth.auth'])
-    if u.present?
-      cookies.encrypted[:user_id] = u.id
-    else
-      # TODO: I18l
-      flash[:error] = '<strong>No user found.</strong>'
-    end
-    redirect_to request.env['omniauth.origin'] || root_url
+    redirect_url = login_redirect_path(request.env['omniauth.origin'])
+    cookies.encrypted[:user_id] = load_user(request.env['omniauth.auth'])
+    redirect_to redirect_url
+  rescue StandardError
+    redirect_to redirect_url, alert: t('.alert', message: help_message)
   end
 
   def destroy
     cookies.encrypted[:user_id] = nil
-
-    # TODO: I18l
-    flash[:success] = '<strong>Logout completed successfully.</strong>'
+    flash.notice = t('.notice')
 
     # Redirect to SSO logout if configured, otherwise root
     strategy = request.env['omniauth.strategy']
     slo_url = strategy.options['idp_slo_service_url'] if strategy.present?
     redirect_to slo_url || root_url
+  end
+
+  private
+
+  def load_user(auth_info)
+    Participant.from_omniauth(auth_info).id
+  end
+
+  def help_message
+    helpers.link_to(t('.help_link'), contact_email_url, class: 'cta')
   end
 end
