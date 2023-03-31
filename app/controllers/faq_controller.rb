e0d80aa4126b85bcbb86f612bf7c30c01e13f44d
Meg Richards <merichar@merichar.org>
2023-03-31 14:57:28 -0400

    Reorder FAQ Private Methods

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -41,16 +41,15 @@ class FAQController < ApplicationController
 
   private
 
-  def update_params
-    params.require(:faq).permit(
-      Current.ability.permitted_attributes(:update, @faq)
-    )
-  end
-
   def create_params
     params.require(:faq).permit(
       Current.ability.permitted_attributes(:create, FAQ)
     )
   end
 
+  def update_params
+    params.require(:faq).permit(
+      Current.ability.permitted_attributes(:update, @faq)
+    )
+  end
 end
