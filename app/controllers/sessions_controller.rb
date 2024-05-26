b5df20e3772edab4d3931bf3fa16c0f66b5b7622
Katelyn Zheng <katelynz@andrew.cmu.edu>
2024-05-26 17:58:46 -0400

    Created dynamic mocking with OmniAuth, changed handling of impersonate in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,36 @@
 # frozen_string_literal: true
 class SessionsController < ApplicationController
+  before_action :setup_mock_auth, if: -> { Rails.env.development? && params[:participant_key] }
+
   def create
-    redirect_url = login_redirect_path(request.env['omniauth.origin'])
-    cookies.encrypted[:user_id] = load_user(request.env['omniauth.auth'])
-    redirect_to redirect_url
-  rescue StandardError
-    redirect_to redirect_url, alert: t('.alert', message: help_message)
+    if Rails.env.development?
+      auth_hash = OmniAuth.config.mock_auth[:shibboleth]
+      participant = Participant.from_omniauth(auth_hash)
+      session[:user_id] = participant.id
+      flash[:notice] = "Logged in as #{participant.name}"
+      redirect_to root_url
+    else
+      redirect_url = login_redirect_path(request.env['omniauth.origin'])
+      begin
+        cookies.encrypted[:user_id] = load_user(request.env['omniauth.auth'])
+        redirect_to redirect_url
+      rescue StandardError
+        redirect_to redirect_url, alert: t('.alert', message: help_message)
+      end
+    end
+  end
+
+  def impersonate
+    participant = Participant.find_by(id: params[:participant_id])
+    if participant
+      cookies.encrypted[:user_id] = participant.id
+      flash[:notice] = "Now impersonating #{participant.name}"
+      redirect_to root_path
+    else
+      redirect_to root_path, alert: "Participant not found."
+    end
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: "Participant not found."
   end
 
   def destroy
