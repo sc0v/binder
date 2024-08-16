02bee2830ccb273a6e9090ec647cb0ec9b1365c8
jmmclaug201 <mclaughlinj201@gmail.com>
2024-08-16 15:13:02 -0400

    Deleted Unused Organization_Status and TappyThing Code, Add support for adding participants to organizations

--- app/helpers/organization_members_helper.rb
+++ app/helpers/organization_members_helper.rb
@@ -1,2 +1,62 @@
+# frozen_string_literal: true
 module OrganizationMembersHelper
+  PARTICIPANTS_TABLE_COLUMNS = [
+    {
+      title: 'Name',
+      field: :name,
+      formatter: 'link',
+      formatterParams: {
+        urlField: :link
+      },
+      frozen: true,
+      headerFilter: 'input',
+      headerFilterPlaceholder: 'Search',
+      sorter: 'string'
+    },
+    {
+      title: 'Andrew Email',
+      field: :eppn,
+      sorter: 'string',
+      headerFilter: 'input',
+      headerFilterPlaceholder: 'Search'
+    },
+    {
+      title: 'Signed Waiver',
+      field: :signed_waiver?,
+      hozAlign: 'center',
+      formatter: 'tickCross',
+      headerFilter: 'tickCross',
+      headerFilterParams: {
+        tristate: true
+      }},
+      {
+      title: 'Booth Chair?',
+      field: :is_booth_chair?,
+      hozAlign: 'center',
+      formatter: 'tickCross',
+      headerFilter: 'tickCross',
+      headerFilterParams: {
+        tristate: true
+    }}
+  ].freeze
+
+  def organization_members_table_config
+    {
+      ajaxURL: organization_participants_path(format: :json),
+      columns: load_participants_columns,
+      dataLoader: false,
+      height: '90vh',
+      paginationSize: 100,
+      placeholder: '<h2>No Members Found</h2>',
+      progressiveLoad: 'load',
+      resizableColumnFit: true
+    }.to_json
+  end
+
+  private
+
+  def load_participants_columns
+    attrs = Current.ability.permitted_attributes(:index, Participant)
+    PARTICIPANTS_TABLE_COLUMNS.select { |c| attrs.include? c[:field] }
+  end
 end
--- app/models/organization_status_type.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class OrganizationStatusType < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-
-  has_many :organization_statuses, dependent: :destroy
-  scope :active,       -> { where(active: true) }
-  scope :inactive,     -> { where(active: false) }
-end
