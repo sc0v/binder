07d538684eabd551c159ae8d195d6fa72c13bc7e
Joe McLaughlin <mclaughlinj201@gmail.com>
2025-02-05 15:33:29 -0500

    Add booth builder bulk add and repair

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -28,6 +28,115 @@ class MembershipsController < ApplicationController
     @organization = Organization.find(params.require(:organization_id))
     @membership = Membership.new
     @membership.organization = @organization
+
+    if params[:step].nil?
+      params[:step] = 'csv_upload'
+    end
+    
+    if params[:step] == 'repair'
+      @members = Membership.where(organization: @organization, is_in_csv: true)
+    end
+  end
+
+  def upload_csv
+    @organization = Organization.find(params[:organization])
+    # Make sure user uploaded a file
+    if params[:csv_file].blank?
+      redirect_to new_organization_membership_path(@organization), alert: 'CSV Not Uploaded!' and return
+    end
+    # Make sure the file parsed successfully
+    parse = helpers.parse_membership_csv(params[:csv_file], @organization)
+    if parse[:error].present?
+      redirect_to new_organization_membership_path(@organization), alert: parse[:error] and return
+    end
+    redirect_to new_organization_membership_path(@organization, step: 'repair')
+  end
+
+  # Insert a participant to the current membership CSV
+  def insert
+    @organization = Organization.find(params[:organization])
+
+    # Find or create new participant
+    new_eppn = "#{params[:new_participant]}@andrew.cmu.edu"
+    new_participant = Participant.find_by(eppn: new_eppn)
+    if new_participant.nil?
+      new_participant = Participant.create!(eppn: new_eppn)
+    end
+    # Create new Membership, or just set is_in_csv if it already exists
+    m = Membership.find_by(participant: new_participant, organization: @organization)
+    if m.nil?
+      m = Membership.create!({
+        participant: new_participant,
+        organization: @organization,
+        is_in_csv: true,
+        is_added_by_csv: true,
+      })
+    else
+      m.update!({is_in_csv: true})
+    end
+  
+    if !params[:ignore_redirect]
+      redirect_to new_organization_membership_path(@organization, step: 'repair')
+    end
+  end
+
+  def remove
+    @organization = Organization.find(params[:organization])
+    # If the membership we're replacing was added by the CSV, destroy it
+    # Otherwise, just remove from the CSV
+    old_membership = Membership.find(params[:old_membership])
+    if old_membership.is_added_by_csv
+      old_participant = old_membership.participant
+      old_membership.destroy!
+      # If old participant now has no memberships, destroy them too
+      if Membership.where(participant: old_participant).blank?
+        old_participant.destroy!
+      end
+    else
+      old_membership.update!({is_in_csv: false})
+    end
+
+    if !params[:ignore_redirect]
+      redirect_to new_organization_membership_path(@organization, step: 'repair')
+    end
+  end
+
+  # Replace one staged membership with another, while respecting any participant's
+  # other memberships
+  def replace
+    # Remove old membership
+    params[:ignore_redirect] = true
+    remove()
+    # Find or create new membership
+    params[:ignore_redirect] = false
+    insert()
+  end
+
+  # Cancel all staged memberships, deleting any that were added by the csv
+  def cancel
+    @organization = Organization.find(params[:organization])
+    Membership.where(organization: @organization, is_in_csv: true).each do |m|
+      if m.is_added_by_csv
+        participant = m.participant
+        m.destroy!
+        # If participant now has no memberships, destroy them too
+        if Membership.where(participant: participant).blank?
+          participant.destroy!
+        end
+      else
+        m.update!({is_in_csv: false})
+      end
+    end
+    redirect_to organization_path(@organization), notice: "Cancelled!"
+  end
+
+  # Commit all staged memberships
+  def commit
+    @organization = Organization.find(params[:organization])
+    Membership.where(organization: @organization, is_in_csv: true).each do |m|
+      m.update!({is_in_csv: false, is_added_by_csv: false})
+    end
+    redirect_to organization_path(@organization), notice: "Participants Added!"
   end
 
   # GET /memberships/1/edit
