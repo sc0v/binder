f936741c28e27e7bcb144784bec89fc2fbaae3f2
Katelyn <katelynz@andrew.cmu.edu>
2025-03-17 12:56:17 -0400

    Fix Organization Show Page
    * Remove charges from organization show page.
    
    * Refactor tool list to dropdown and show more information.
    
    * Refactor members list for organizations show page to separate booth chairs and builders into separate dropdown lists.
    
    * Turn links for structural and electrical progress into buttons.
    
    * Fix abilities check for add members button visibility.
    
    * Add comments for code documentation for each part of organization show page.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -23,10 +23,10 @@ class OrganizationsController < ApplicationController
 
   def show
     @booth_chairs = @organization.booth_chairs
+    @non_booth_chairs = @organization.validated_non_booth_chairs
     @tools = Tool.checked_out_by_organization(@organization).just_tools
     @shifts = @organization.shifts
     @participants = @organization.validated_participants
-    @charges = @organization.charges
 
       pagy, participants =
         pagy(@organization.participants.accessible_by(Current.ability).ordered_by_name)
@@ -45,11 +45,6 @@ class OrganizationsController < ApplicationController
     @structural = @organization.organization_build_statuses.find_by(status_type: :structural)
     @electrical = @organization.organization_build_statuses.find_by(status_type: :electrical)
 
-    # Get Total Fines for Organization
-    @total_fines = 0
-    @organization.charges.each do |charge|
-      @total_fines += charge.amount
-    end
     # Get Tools Checked Out by Organization
     @tools_checked_out = Tool.checked_out_by_organization(@organization)
   end
