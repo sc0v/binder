c9d4bb7db15eb80a8ad20f077ec1edcfc47126ad
Katelyn Zheng <katelynz@andrew.cmu.edu>
2025-03-16 21:58:18 -0400

    Remove charges from organization show page.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -23,7 +23,6 @@ class OrganizationsController < ApplicationController
     @tools = Tool.checked_out_by_organization(@organization).just_tools
     @shifts = @organization.shifts
     @participants = @organization.validated_participants
-    @charges = @organization.charges
 
       pagy, participants =
         pagy(@organization.participants.accessible_by(Current.ability).ordered_by_name)
@@ -42,11 +41,6 @@ class OrganizationsController < ApplicationController
     @structural = @organization.organization_build_statuses.find_by(status_type: :structural)
     @electrical = @organization.organization_build_statuses.find_by(status_type: :electrical)
 
-    # Get Total Fines for Organization
-    @total_fines = 0
-    @organization.charges.each do |charge|
-      @total_fines += charge.amount
-    end
     # Get Tools Checked Out by Organization
     @tools_checked_out = Tool.checked_out_by_organization(@organization)
   end
