7a89a29b6dbff80000432a56ecb49f3dadf035b3
helenllii <164818597+helenllii@users.noreply.github.com>
2025-03-21 18:30:03 -0400

    tooltypes (#338)
    * tooltypes
    
    * tool types (delete unnecessary lines)
    
    ---------
    
    Co-authored-by: Joe McLaughlin <jmmclaug@andrew.cmu.edu>

--- config/routes.rb
+++ config/routes.rb
@@ -94,6 +94,9 @@ Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
     resources :checkouts, controller: 'tools/checkouts'
   end
 
+  # Tool types
+  resources :tool_types 
+
   # Tool Inventory
   resources :tool_inventory, path: 'inventory', only: [:show] do
     resources :tool_inventory_tools, only: %i[new create index destroy]
