042118468535fb9c8aa5f9d3740b2e20c166ab84
natsuki615 <carinazheng949@GMAIL.com>
2026-02-18 01:36:18 +0000

    fix: run db:migrate to apply changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_08_185252) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_08_185252) do
   create_table "certification_types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
