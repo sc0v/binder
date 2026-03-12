a997b358240722835d970db10b5c1d984b0d0d0d
Katie Zhang <zhangmy667@gmail.com>
2026-03-12 21:43:14 +0000

    feat: add NotificationSubscription model

---
+++ app/models/notification_subscription.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class NotificationSubscription < ApplicationRecord
+  belongs_to :participant
+
+  validates :participant_id, :endpoint, presence: true
+  validates :endpoint, uniqueness: true
+
+  scope :active, -> { where(active: true) }
+  scope :inactive, -> { where(active: false) }
+end
