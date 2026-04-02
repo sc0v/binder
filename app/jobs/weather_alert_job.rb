fb59f942cf3eb7cda5ba8ad029de7d2461baf561
Meg Richards <merichar@merichar.org>
2026-04-01 22:02:17 -0400

    fix(jobs): define WeatherAlertJob class to satisfy Zeitwerk autoloading
    * file existed but contained only a comment, causing a Zeitwerk::NameError on boot
    * adds stub class with empty perform method until implementation is ready

--- app/jobs/weather_alert_job.rb
+++ app/jobs/weather_alert_job.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
 # polls for emergency weather alerts and sends notifications to users
+class WeatherAlertJob < ApplicationJob
+  queue_as :default
+
+  def perform(*_args)
+    # TODO: implement weather alert polling and notifications
+  end
+end
