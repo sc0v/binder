397a3e77961e4a77e5302bf5b722e1e9e82f9ec3
Meg Richards <merichar@merichar.org>
2026-04-01 21:32:42 -0400

    fix(push): allow boot without VAPID keys for asset compilation

--- config/initializers/push_notifications.rb
+++ config/initializers/push_notifications.rb
@@ -6,7 +6,7 @@
 vapid_subject_default =
   "mailto:#{Rails.application.config.try(:support_email) || 'support@example.com'}"
 
-Rails.application.config.vapid_public_key = ENV.fetch('VAPID_PUBLIC_KEY')
-Rails.application.config.vapid_private_key = ENV.fetch('VAPID_PRIVATE_KEY')
+Rails.application.config.vapid_public_key = ENV.fetch('VAPID_PUBLIC_KEY', nil)
+Rails.application.config.vapid_private_key = ENV.fetch('VAPID_PRIVATE_KEY', nil)
 Rails.application.config.vapid_subject =
   ENV.fetch('VAPID_SUBJECT', vapid_subject_default)
