0edb8145ab5f82893fef2c8e0adfbb6bb391b1bc
Katie Zhang <zhangmy667@gmail.com>
2026-04-04 15:45:02 -0400

    fix: resolve error when making participant alumni

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -52,7 +52,7 @@ class Participant < ApplicationRecord
            dependent: :destroy
   has_many :organizations, through: :memberships
   has_many :organization_categories, through: :organizations
-  has_many :organization_build_steps, dependent: :restrict_with_error
+  has_many :organization_build_steps, foreign_key: :approver_id, dependent: :restrict_with_error
   has_many :certifications, dependent: :destroy
   has_many :certification_types, through: :certifications
 
